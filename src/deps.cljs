f796e169d8913a41653a04f993befdf183951323
Simon Skorokhodov <skorohodovsemen@gmail.com>
2021-03-02 12:01:51 +0100

    update react-native-tcp-socket to 4.5.7

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,2 +1,2 @@
 {:npm-deps {"@react-native-async-storage/async-storage" "1.14.1"
-            "react-native-tcp-socket"               "4.5.1"}}
+            "react-native-tcp-socket"               "4.5.7"}}
